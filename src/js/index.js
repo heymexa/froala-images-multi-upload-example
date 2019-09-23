5ffb5574589ab0d49393556d74390d8186b4c51c
heymexa <heymexa@gmail.com>
2019-09-23 23:00:29 +0300

    create a demo page with froala editor v2.x

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,18 @@
-import FroalaEditor from 'froala-editor';
-
-import 'froala-editor/js/plugins/align.min.js';
+import $ from 'jquery';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/js/froala_editor.pkgd.min';
+import 'froala-editor/js/plugins/image.min';
 
-new FroalaEditor('#froala-editor');
+import 'froala-images-multi-upload/build/js/imagesMultiUpload';
+import 'froala-images-multi-upload/build/css/imagesMultiUpload.css';
+
+window.$ = window.jQuery = $;
+
+$('#froala-editor').froalaEditor({
+  // Set image buttons, including the name
+  // of the buttons defined in customImageButtons.
+  imageEditButtons: ['imageDisplay', 'imageAlign', 'imageInfo', 'imageRemove'],
+  imageUploadURL: 'http://i.froala.com/upload',
+  toolbarButtons: ['imagesMultiUpload']
+});
