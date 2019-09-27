d689776f06a320878ed1843b3c29fa9916940df4
heymexa <heymexa@gmail.com>
2019-09-27 23:03:17 +0300

    add example for Froala Editor v3

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,15 @@
 import FroalaEditor from 'froala-editor';
 
-import 'froala-editor/js/plugins/align.min.js';
+import 'froala-editor/js/third_party/font_awesome.min';
+import 'froala-editor/js/plugins/image.min';
 import 'froala-editor/css/froala_editor.pkgd.min.css';
 
-new FroalaEditor('#froala-editor');
+import 'froala-images-multi-upload/build/js/imagesMultiUpload';
+import 'froala-images-multi-upload/build/css/imagesMultiUpload.css';
+
+new FroalaEditor('#froala-editor', {
+  imageEditButtons: ['imageDisplay', 'imageAlign', 'imageInfo', 'imageRemove'],
+  imageUploadURL: 'http://i.froala.com/upload',
+  toolbarButtons: ['imagesMultiUpload'],
+  pluginsEnabled: ['image', 'imagesMultiUpload']
+});
